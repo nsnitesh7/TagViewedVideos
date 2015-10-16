8d9aeecdf6a62ebf00ac31beb4bd20898dd10f1e
Nitesh Singhal <nsnitesh7@gmail.com>
2015-10-17 00:37:27 +0530

    Tag Videos in a folder
    The application tells which of the videos have already been watched and
    which ones are left.

---
+++ TagViewedVideos/FileList.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace TagViewedVideos
+{
+    public class FileList
+    {
+        private string historyFilePath;
+        public FileList()
+        {
+            Files = new ObservableCollection<FileInfo>();
+            NotViewedFiles = new ObservableCollection<FileInfo>();
+        }
+
+        public ObservableCollection<FileInfo> Files { get; private set; }
+
+        public ObservableCollection<FileInfo> NotViewedFiles { get; private set; }
+
+        public string Path { get; set; }
+
+
+        private HashSet<string> ReadHistoryFile()
+        {
+
+            var list = new HashSet<string>();
+
+            using (StreamReader sr = new StreamReader(historyFilePath))
+            {
+                while (sr.Peek() >= 0)
+                {
+                    list.Add(sr.ReadLine());
+                }
+            }
+            return list;
+        }
+
+        public void RefreshFiles()
+        {
+            try
+            {
+                Files.Clear();
+                NotViewedFiles.Clear();
+
+                historyFilePath = Path + @"\ViewedHistory.txt";
+
+                HashSet<string> viewedHistoryFilesList = new HashSet<string>();
+
+                if (File.Exists(historyFilePath))
+                {
+                    viewedHistoryFilesList = ReadHistoryFile();
+                }
+
+                foreach (FileInfo file in new DirectoryInfo(Path).GetFiles())
+                {
+                    if (string.Equals(file.Name, "ViewedHistory.txt")) { continue; }
+
+                    if (viewedHistoryFilesList.Contains(file.Name))
+                    {
+                        Files.Add(file);
+                    }
+                    else
+                    {
+                        NotViewedFiles.Add(file);
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show("Unable to read folder", "Error",
+                    MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        internal void UpdateHistoryFile()
+        {
+            if (!File.Exists(historyFilePath))
+            {
+                File.CreateText(historyFilePath).Close();
+                //File.SetAttributes(historyFilePath, FileAttributes.Normal | FileAttributes.Hidden);
+            }
+
+            using (StreamWriter sw = new StreamWriter(historyFilePath))
+            {
+                foreach (FileInfo f in Files)
+                {
+                    sw.WriteLine(f.Name);
+                }
+            }
+        }
+    }
+}
---
+++ TagViewedVideos/MainWindow.xaml.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Drawing;
+using WinForms = System.Windows.Forms;
+using System.IO;
+using System.Collections;
+
+namespace TagViewedVideos
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = new FileList();
+        }
+
+        private void pickFolderButton_Click(object sender, RoutedEventArgs e)
+        {
+            var dialog = new WinForms.FolderBrowserDialog();
+            WinForms.DialogResult result = dialog.ShowDialog();
+            folderLocationTextBox.Text = dialog.SelectedPath;
+
+            var dc = DataContext as FileList;
+            dc.Path = folderLocationTextBox.Text;
+            dc.RefreshFiles();
+        }
+
+        private void viewedClickHandler(object sender, RoutedEventArgs e)
+        {
+            var dc = DataContext as FileList;
+            var selectedFiles = (notViewedListView.SelectedItems).Cast<FileInfo>().ToList();
+            foreach (FileInfo f in selectedFiles)
+            {
+                dc.NotViewedFiles.Remove(f);
+                dc.Files.Add(f);
+            }
+            dc.UpdateHistoryFile();
+        }
+
+        private void notViewedClickHandler(object sender, RoutedEventArgs e)
+        {
+            var dc = DataContext as FileList;
+            var selectedFiles = (viewedListView.SelectedItems).Cast<FileInfo>().ToList();
+            foreach (FileInfo f in selectedFiles)
+            {
+                dc.Files.Remove(f);
+                dc.NotViewedFiles.Add(f);
+            }
+            dc.UpdateHistoryFile();
+        }
+    }
+}
